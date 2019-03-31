97efe2dfd7685e403647850e20db206d7e71d5b1
d-rea-mer <44610017+d-rea-mer@users.noreply.github.com>
2019-03-31 14:20:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,31 @@ bot.on("message", (msg,) => {
    else if(msg.text=="How are you?" || msg.text=="How r u?"){
        bot.sendMessage(chatId,"Ha HA fine...");
    }
-   else if (msg.text=="I love you" || msg.text=="I love u"){ 
+   else if (msg.text=="entha parupadi"){ 
         
-   bot.sendMessage(chatId,"love u toooo");
+   bot.sendMessage(chatId,"veruthe rest eduka");
    }
+    else if(msg.text=="source code send"){
+    bopt.sendMessage(chatId,"const TelegramBot = require('node-telegram-bot-api');
+
+const port = process.env.PORT || 443,
+    host = '0.0.0.0', // probably this change is not required
+    externalUrl = 'https://terbo.herokuapp.com',
+    token = process.env.TOKEN || '832140817:AAFZJxqITBiXcby0FVGQ9KEtNkik3yIFDAo',
+    bot = new TelegramBot(token, {
+        webHook: {
+            port: port,
+            host: host
+        }
+    });
+bot.setWebHook(externalUrl + ':443/bot' + token);
+bot.on("message", (msg,) => {
+ 
+  const chatId = msg.chat.id 
+  if(msg.text=="Hai" || msg.text=="Hello"){
+  bot.sendMessage(chatId, "hello");
+  }");
+   
     else{
        bot.sendMessage(chatId,"poda");
    }
