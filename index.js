256c40e1520c3bb740b022eb0b6d593fbdef176b
Sandeep S <dxsandeeps@gmail.com>
2019-03-31 11:03:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const port = process.env.PORT || 443,
+    host = '0.0.0.0', // probably this change is not required
+    externalUrl = 'YOUR HEROKU URL',
+    token = process.env.TOKEN || 'YOUR TELEGRAM TOKEN',
+    bot = new TelegramBot(token, {
+        webHook: {
+            port: port,
+            host: host
+        }
+    });
+bot.setWebHook(externalUrl + ':443/bot' + token);
