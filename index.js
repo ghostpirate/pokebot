659c473f19e413d06c5ce41a616c0a8a8aa388e5
d-rea-mer <44610017+d-rea-mer@users.noreply.github.com>
2019-03-31 14:23:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,26 +24,6 @@ bot.on("message", (msg,) => {
         
    bot.sendMessage(chatId,"veruthe rest eduka");
    }
-    else if(msg.text=="source code send"){
-    bopt.sendMessage(chatId,"const TelegramBot = require('node-telegram-bot-api');
-
-const port = process.env.PORT || 443,
-    host = '0.0.0.0', // probably this change is not required
-    externalUrl = 'https://terbo.herokuapp.com',
-    token = process.env.TOKEN || '832140817:AAFZJxqITBiXcby0FVGQ9KEtNkik3yIFDAo',
-    bot = new TelegramBot(token, {
-        webHook: {
-            port: port,
-            host: host
-        }
-    });
-bot.setWebHook(externalUrl + ':443/bot' + token);
-bot.on("message", (msg,) => {
- 
-  const chatId = msg.chat.id 
-  if(msg.text=="Hai" || msg.text=="Hello"){
-  bot.sendMessage(chatId, "hello");
-  }");
    
     else{
        bot.sendMessage(chatId,"poda");
