a55f1cb6b7222b08666b30274fc6dec5ff9409fb
Govind Kartha <44610432+govkartha@users.noreply.github.com>
2019-03-31 11:35:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const port = process.env.PORT || 443,
     host = '0.0.0.0', // probably this change is not required
-    externalUrl = process.env.HOSTURL,
-    token = process.env.TOKEN || 'YOUR TELEGRAM TOKEN',
+    externalUrl = "https://pikachu-1.herokuapp.com",
+    token = process.env.TOKEN || '840524827:AAFrTvc5ifZ2re6NdIwrMNUrrtLaGjEeS8A',
     bot = new TelegramBot(token, {
         webHook: {
             port: port,
